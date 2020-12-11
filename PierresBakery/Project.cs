e0e03627c82880926a5737d15053e4e536281a5c
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-11 14:21:18 -0800

    Add main() code to read user input and return answer

--- PierresBakery/Project.cs
+++ PierresBakery/Project.cs
@@ -1,22 +1,24 @@
 using System;
 using System.Collections.Generic;
 
-namespace PierreBakery.Models
+namespace PierresBakery.Models
 {
   public class Program
   {
     public static void Main()
     {
-      //Console.WriteLine("Welcome to Pierre's Bakery!");
-      //Console.WriteLine("Bread is $5 a loaf, but there is a sale for buy 2 get one free!");
-      //Console.WriteLine("Pastries are $2 each, but also on a sale 3 for $5!");
-      //Console.WriteLine("How many loaves of bread would you like to buy?");
-      //int bread = Console.ReadLine();
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Bread is $5 a loaf, but there is a sale for buy 2 get one free!");
+      Console.WriteLine("Pastries are $2 each, but also on a sale 3 for $5!");
+      Console.WriteLine("How many loaves of bread would you like to buy?");
+      int breadAmmount = int.Parse(Console.ReadLine());
+      Bread breadClass = new Bread(breadAmmount);
 
-      //Console.WriteLike("How many pastries would you like?");
-      //int pastry = Console.ReadLine();
+      Console.WriteLine("How many pastries would you like?");
+      int pastryAmmount = int.Parse(Console.ReadLine());
+      Pastry pastryClass = new Pastry(pastryAmmount);
 
-      //Console.WriteLine("Your total today is: " );
+      Console.WriteLine("Your total today is: " + (breadClass.AddRemainder() + pastryClass.AddRemainder()) + ".");
     }
   }
 }
