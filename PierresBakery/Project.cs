2d1f8f3d751a2040bbe6268a3f85d78e5b369e8d
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-11 16:40:01 -0800

    Add functionality to make multible orders

--- PierresBakery/Project.cs
+++ PierresBakery/Project.cs
@@ -7,25 +7,51 @@ namespace PierresBakery.Models
   {
     public static void Main()
     {
+      List<int> totals = new List<int> {};
+      bool buyMore = true;
+      int counter = 0;
+
       Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("Bread is $5 a loaf, but there is a sale for buy 2 get one free!");
-      Console.WriteLine("Pastries are $2 each, but also on a sale 3 for $5!");
-      Console.WriteLine("How many loaves of bread would you like to buy?");
-      int breadAmmount = int.Parse(Console.ReadLine());
-      Bread breadClass = new Bread(breadAmmount);
+      while (buyMore == true)
+      {
+        Console.WriteLine("Bread is $5 a loaf, but there is a sale for buy 2 get one free!");
+        Console.WriteLine("Pastries are $2 each, but also on a sale 3 for $5!");
+        Console.WriteLine("How many loaves of bread would you like to buy?");
+        int breadAmmount = int.Parse(Console.ReadLine());
+        Bread breadClass = new Bread(breadAmmount);
 
-      Console.WriteLine("How many pastries would you like?");
-      int pastryAmmount = int.Parse(Console.ReadLine());
-      Pastry pastryClass = new Pastry(pastryAmmount);
+        Console.WriteLine("How many pastries would you like?");
+        int pastryAmmount = int.Parse(Console.ReadLine());
+        Pastry pastryClass = new Pastry(pastryAmmount);
 
-      Console.WriteLine("Your total for bread is: $" + breadClass.AddRemainder() + ".");
-      Console.WriteLine("And your total for pastries is: $" + pastryClass.AddRemainder() + ".");
-      Console.WriteLine("Your total today is: $" + (breadClass.AddRemainder() + pastryClass.AddRemainder()) + ".");
+        totals.Add(breadClass.AddRemainder() + pastryClass.AddRemainder());
+
+        Console.WriteLine("Your total for bread is: $" + breadClass.AddRemainder() + ".");
+        Console.WriteLine("And your total for pastries is: $" + pastryClass.AddRemainder() + ".");
+        Console.WriteLine("Your total for both is: $" + totals[counter] + ".");
+        Console.WriteLine("Would you like to buy more? Y/N");
+        string buyMoreAnswer = Console.ReadLine();
+
+        if (buyMoreAnswer == "Y")
+        {
+          buyMore = true;
+          counter++;
+        } else
+        {
+          int endTotal = 0;
+          for (int i = 0; i <= counter; i++)
+          {
+            endTotal += totals[i];
+          }
+          Console.WriteLine("Your total for both is: $" + endTotal + ".");
+          buyMore = false;
+        }
+      }
 
       Console.WriteLine("Would you like to shop again? Y/N");
-      string choice = Console.ReadLine();
+      string continueAnswer = Console.ReadLine();
 
-      if (choice == "Y")
+      if (continueAnswer == "Y")
       {
         Program.Main();
       } else
